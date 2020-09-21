09eed6f171715909c091c95fedb98c98b5dbd03a
NoZ-design <68496572+NoZ-design@users.noreply.github.com>
2020-09-21 15:10:13 +0800

    20200921C_sharp

---
+++ 20200921/方案總管從右邊改.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _20200921
+{
+    class 方案總管從右邊改
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Programing!");
+        }
+    }
+}
